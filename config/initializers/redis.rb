82396575c93ad108bc79c7263e41a07d16f537ba
Kotsiuruba Ruslan <ruslan.kotsuruba@gmail.com>
2016-07-07 21:39:39 +0300

    change redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-$redis = Redis::Namespace.new("chat_api", :redis => Redis.new)
+if ENV["REDISCLOUD_URL"]
+  $redis_instance = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  $redis_instance = Redis.new
+end
+$redis = Redis::Namespace.new("chat_api", :redis => $redis_instance)
